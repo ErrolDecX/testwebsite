49333b2d53590e6f3d9f0c2ab8e0373471e14f6b
ErrolDecX <erroldecx@gmail.com>
2024-11-24 10:40:03 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var audio = document.getElementById("background-audio");
-    var playPromise = audio.play();
+    audio.play();
 
-    if (playPromise !== undefined) {
-        playPromise.then(function() {
-            console.log("Audio is playing.");
-        }).catch(function(error) {
-            console.error("Audio playback failed:", error);
-        });
-    }
+    setTimeout(function() {
+        audio.muted = false;
+    }, 1000); // 1 second delay to unmute
 });
